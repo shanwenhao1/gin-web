441782e8a103a472fb42a786cc89c1de5b85f558
swh <761542858@qq.com>
2019-02-25 16:10:42 +0800

    fix bug of app action

--- dddProject/app/action/action_user.go
+++ dddProject/app/action/action_user.go
@@ -15,7 +15,7 @@ type UserJsonModel struct {
 // 登录请求
 func (this UserJsonModel) Login(c *gin.Context) {
 	// 获取请求参数, 可以考虑在此添加中间件
-	rjm := service.GetRequestData(c, UserJsonModel{})
+	rjm := service.GetRequestData(c, &model.RequestJsonModel{})
 	if rjm != nil {
 		jsonModel := *rjm.(*model.RequestJsonModel)
 		// 可以做一些验证
